a6078ac977700d8d4f6296c56e5d1857c0e60326
Brian M. Carr <delphyne@gmail.com>
2010-08-25 14:23:37 -0500

    add ignores, mindmap, zip descriptor for packaging

---
+++ src/site/resources/rcli.mm
@@ -0,0 +1,24 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1282348381890" ID="Freemind_Link_626746425" MODIFIED="1282348392966" TEXT="rcli">
+<node CREATED="1282348393902" ID="_" MODIFIED="1282348412748" POSITION="right" TEXT="Making a raid">
+<node CREATED="1282348413472" ID="Freemind_Link_935307831" MODIFIED="1282348417220" TEXT="convert"/>
+<node CREATED="1282348418184" ID="Freemind_Link_1062653273" MODIFIED="1282348446051" TEXT="invite &lt;player&gt; [...]"/>
+<node CREATED="1282348456631" ID="Freemind_Link_1535958291" MODIFIED="1282348476507" TEXT="kick &lt;player&gt; [...]"/>
+</node>
+<node CREATED="1282348806515" ID="Freemind_Link_1304833519" MODIFIED="1282348813984" POSITION="left" TEXT="Promote a player">
+<node CREATED="1282348815779" ID="Freemind_Link_1013703664" MODIFIED="1282348826150" TEXT="promote &lt;player&gt;"/>
+<node COLOR="#cc3300" CREATED="1282348827002" ID="Freemind_Link_1055720173" MODIFIED="1282354406976" TEXT="promote &lt;player&gt; tank"/>
+<node CREATED="1282348837706" ID="Freemind_Link_704404184" MODIFIED="1282348847181" TEXT="promote &lt;player&gt; leader"/>
+<node CREATED="1282348848114" ID="Freemind_Link_214325680" MODIFIED="1282348855293" TEXT="promote &lt;player&gt; looter"/>
+<node COLOR="#cc3300" CREATED="1282348855769" ID="Freemind_Link_1880600488" MODIFIED="1282354412538" TEXT="promote &lt;player&gt; assist"/>
+</node>
+<node CREATED="1282349400464" ID="Freemind_Link_1073307360" MODIFIED="1282349405395" POSITION="right" TEXT="Raid Organization">
+<node CREATED="1282349406151" ID="Freemind_Link_1556208165" MODIFIED="1282349416666" TEXT="move &lt;player&gt; &lt;group number&gt;"/>
+<node CREATED="1282349417535" ID="Freemind_Link_325898000" MODIFIED="1282349425467" TEXT="swap &lt;player&gt; &lt;player&gt;"/>
+</node>
+<node CREATED="1282358836270" ID="Freemind_Link_731060463" MODIFIED="1282358840985" POSITION="left" TEXT="Demote a player">
+<node CREATED="1282358842013" ID="Freemind_Link_1802526188" MODIFIED="1282358847593" TEXT="demote &lt;player&gt;"/>
+</node>
+</node>
+</map>

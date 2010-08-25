65b925649f4cc6e5dc54ebaead196945995986c0
Brian M. Carr <delphyne@gmail.com>
2010-08-25 13:28:05 -0500

    adjustments to "inparty" detection, corrected a namespace issue with table code in promote, added demote functionality, safer error messages when looking up names

--- src/main/lua/rcli.lua
+++ src/main/lua/rcli.lua
@@ -1,27 +1,19 @@
 local RCLI = CreateFrame("Frame", "RCLI")
 
-local ConvertOnPartyJoin = false
-
-local function InParty()
-    return GetNumPartyMembers() == 0
-end
-
-local function InRaid()
-    return GetNumRaidMembers() ~= 0
-end
+local convertOnPartyJoin = false
 
 local function FindRaidIndex(player)
-    for i=1,GetNumRaidMembers() do
-        name, rank, subgroup = GetRaidRosterInfo(i)
-        if name == player then
+    for i = 1, GetNumRaidMembers() do
+        local name, rank, subgroup = GetRaidRosterInfo(i)
+        if name:upper() == player:upper() then
             return i, subgroup
         end
     end
 end
 
 local function Promote(args)
-    local player = tbl.remove(args, 1)
-    local role = tbl.remove(args,1)
+    local player = table.remove(args, 1)
+    local role = table.remove(args,1)
     
     if player == nil or player == "" then
         print("You must specify a player to promote.")
@@ -36,12 +28,26 @@ local function Promote(args)
     end
 end
 
+local function Demote(args)
+    local player = table.remove(args, 1)
+    if player == nil or player == "" then
+        print("You must specify a player to demote.")
+    else
+        DemoteAssistant(player, false)
+        local method, partyMaster, raidMaster = GetLootMethod()
+        local index = FindRaidIndex(player)
+        if method == "master" and index == raidMaster then
+            SetLootMethod("master", "player")
+        end
+    end
+end
+
 local function Convert()
-    if InParty() then
+    if not UnitInParty("player") then
         print("You are not in a party.")
     elseif IsPartyLeader() ~= 1 then
         print("You are not the party leader.")
-    elseif InRaid() then
+    elseif not UnitInRaid("player") then
         print("You are already in a raid.")
     else
         ConvertToRaid()
@@ -50,7 +56,7 @@ end
 
 local function Kick(args)
     local player = table.remove(args, 1)
-    if player == nil or player ~= "" then
+    if player == nil or player == "" then
         print("You must specify a player to kick.")
     else
         UninviteUnit(player)
@@ -58,9 +64,7 @@ local function Kick(args)
 end
 
 local function Invite(args)
-    if InParty() then
-        ConvertToRaid()
-    elseif not InRaid() then
+    if not UnitInRaid("player") then
         convertOnPartyJoin = true
     end
     
@@ -76,7 +80,7 @@ local function Move(args)
     local raidindex, curgroup = FindRaidIndex(player)
     
     if curgroup == group then
-        print(player .. " is already in group " .. group .. ".")
+        print(player, "is already in group", group, ".")
     else
         SetRaidSubgroup(raidindex, group)
     end
@@ -85,12 +89,16 @@ end
 local function Swap(args)
     local player1 = table.remove(args, 1)
     local player2 = table.remove(args, 1)
+    
     local raidindex1 = FindRaidIndex(player1)
+    print("player1", player1, "index1", raidindex1)
     local raidIndex2 = FindRaidIndex(player2)
+    print("player2", player2, "index2", raidindex2)
+
     if raidindex1 == nil then
-        print(player1, " is not in the raid.")
+        print(player1, "is not in the raid.")
     elseif raidindex2 == nil then 
-        print(player2, " is not in the raid.")
+        print(player2, "is not in the raid.")
     elseif raidindex1 == raidindex2 then
         print("You must specify two players to swap.")
     else
@@ -116,25 +124,27 @@ local function ParseCli(msg)
         Move(args)
     elseif cmd == "swap" then
         Swap(args)
+    elseif cmd == "demote" then
+        Demote(args)
     end
 end
 
 SLASH_RCLI1 = "/rcli"
-SlashCmdList["RCLI"] = function(msg)
-    ParseCli(msg)
-end
+SlashCmdList["RCLI"] = ParseCli
 
 local function HandlePartyJoin(self, event, ...)
-    if convertOnPartyJoin and event == "PARTY_MEMBERS_CHANGED" then
-        convertOnPartyJoin = false
-        ConvertToRaid()
+    if convertOnPartyJoin then
+        if not UnitInRaid("player") then
+            convertOnPartyJoin = false
+            ConvertToRaid()
+        end
     end
 end
 
 SLASH_RELOAD1 = "/rl"
-SlashCmdList["RELOAD"] = ParseCli
+SlashCmdList["RELOAD"] = function(msg)
+    ReloadUI()
+end
 
 RCLI:SetScript("OnEvent", HandlePartyJoin)
 RCLI:RegisterEvent("PARTY_MEMBERS_CHANGED")
-
-print("foo")
